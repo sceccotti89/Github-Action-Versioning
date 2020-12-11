d8b774dfa5de93a04ce696ad7e162bef6143ae17
Stefano <stefano@stefanos-mbp.home>
2020-12-11 11:36:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const version = '1.0.0';
+const BRANCH_REF = 'ref/heads/';
+const TAG_REF    = 'ref/tags/';
 
 try {
-    //const payload = JSON.stringify(github.context.payload, undefined, 2);
-    //console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 
     const ref = github.context.ref;
-    const branch = ref.substr(ref.lastIndexOf('/') + 1);
-    console.log('Branch:', branch);
-
     const sha = github.context.sha.substr(0, 8);
+    let version_name = '';
+
+    if (isMainBranchOrTag(ref)) {
+        version_name = `${version}`;
+    } else {
+        const branch = ref.substr(ref.indexOf(BRANCH_REF) + 1);
+        version_name = `${branch}-${version}-${sha}`;
+    } 
+
+
     
     const finalVersion = `${branch}-${version}-${sha}`;
 
     core.setOutput("version", finalVersion);
 } catch (error) {
     core.setFailed(error.message);
+}
+
+const isMainBranchOrTag = (ref) => {
+    if (ref.startsWith(TAG_REF) || (ref.startsWith(BRANCH_REF) && ref.startsWith(`${BRANCH_REF}main`))) {
+        return true;
+    }
+    return false;
 }
