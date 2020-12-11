86844a25117bf38ad87d8eef381cb95293e90c80
Stefano <stefano@stefanos-mbp.home>
2020-12-11 11:56:16 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -16,6 +16,7 @@ try {
     const payload = JSON.stringify(github.context, undefined, 2);
     console.log(`The event payload: ${payload}`);
 
+    console.log('NEW VERSION:', incrementPatchVersion(BASE_VERSION));
     const version = (isPullRequest(github.context.payload)) ? incrementPatchVersion(BASE_VERSION) : BASE_VERSION ;
 
     const ref = github.context.ref;
