ff4a34029453f2e6e5b9ca4c83ed3d2df5a9fe02
Stefano <stefano@stefanos-mbp.home>
2020-12-11 10:55:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  core.setOutput("version", '1.0.0');
-
-  console.log(`Ref:`, github.context.ref);
-
-  const payload = JSON.stringify(github.context, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+    console.log(`Ref:`, github.context.ref);
+    
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+    
+    core.setOutput("version", '1.0.0');
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
